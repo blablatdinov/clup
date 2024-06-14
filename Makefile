4162890060dae924b9c62e1e9108a8c494818ab3
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2024-06-15 01:25:05 +0300

    Initial

---
+++ Makefile
@@ -0,0 +1,38 @@
+# The MIT License (MIT).
+#
+# Copyright (c) 2024 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
+lint:
+	poetry run isort clup tests
+	poetry run ruff check clup tests --fix
+	poetry run flake8 clup tests
+	poetry run mypy clup tests --strict
+
+test: unit it
+
+unit:
+	poetry run pytest tests/unit --cov=clup --cov-report=term-missing:skip-covered -vv
+
+it:  # integration tests
+	poetry run pytest tests/it -s -vv
+
+clean:
+	git clean -f -d -x
---
+++ clup/clup.py
@@ -0,0 +1,41 @@
+# The MIT License (MIT).
+#
+# Copyright (c) 2024 Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+
+ChangelogStr = str
+NewVersionNumStr = str
+DateStr = str
+
+
+def main(changelog: ChangelogStr, version: NewVersionNumStr, date: DateStr) -> ChangelogStr:
+    res = []
+    for line in changelog.splitlines():
+        if line == '## [Unreleased]':
+            res.append('## [Unreleased]\n')
+            res.append('## [{0}] - {1}'.format(version, date))
+            continue
+        if line.startswith('[unreleased]: '):
+            # TODO: hardcoded lines
+            res.append('[unreleased]: https://github.com/olivierlacan/keep-a-changelog/compare/1.1.2...HEAD')
+            res.append('[1.1.2]: https://github.com/olivierlacan/keep-a-changelog/compare/1.1.1...1.1.2')
+            continue
+        res.append(line)
+    return '\n'.join(res) + '\n'
